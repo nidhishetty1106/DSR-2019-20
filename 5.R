57fbcbd8ca1a0677570c971695870eeaf234fbe8
nidhishetty1106 <39126525+nidhishetty1106@users.noreply.github.com>
2019-11-13 16:23:09 +0530

    Add files via upload

---
+++ 5.R
@@ -0,0 +1,7 @@
+x <- rnorm(10, mean = 100, sd = 5)
+y <- rnorm(20, mean = 105, sd = 5)
+
+qt(p = 0.05/2, df = 28, lower.tail = FALSE)
+t.test(x, y, var.equal = TRUE)
+print("Suhas M Suresh")
+print("Batch - C2")
